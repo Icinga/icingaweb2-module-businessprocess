ad96b0c2aa04cd07de32932217382896e5339890
Thomas Gelf <thomas@gelf.net>
2014-11-30 10:54:48 +0100

    menu: move from availabilty to reporting

--- configuration.php
+++ configuration.php
@@ -1,8 +1,6 @@
 <?php
 
-$section = $this->menuSection($this->translate('Availability'), array(
-    'icon'     => 'img/icons/servicegroup.png',
-    'priority' => 40
-));
-$section->add($this->translate('Business Processes'))->setUrl('bpapp/process/show');
+$section = $this->menuSection($this->translate('Reporting'))
+    ->add($this->translate('Business Processes'))
+    ->setUrl('bpapp/process/show');
 
