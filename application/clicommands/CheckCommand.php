e52037b84f144af47d5b9f954807ae44b8920de0
Thomas Gelf <thomas@gelf.net>
2015-11-19 14:46:36 +0100

    cli/check: add --details and --colors

--- application/clicommands/CheckCommand.php
+++ application/clicommands/CheckCommand.php
@@ -4,10 +4,27 @@ namespace Icinga\Module\Businessprocess\Clicommands;
 
 use Icinga\Cli\Command;
 use Icinga\Module\Businessprocess\Storage\LegacyStorage;
+use Icinga\Module\Businessprocess\BpNode;
+use Icinga\Module\Businessprocess\HostNode;
 
 class CheckCommand extends Command
 {
     protected $storage;
+
+    protected $hostColors = array(
+        0 => array('black', 'lightgreen'),
+        1 => array('black', 'lightred'),
+        2 => array('black', 'brown'),
+        99 => array('black', 'lightgray'),
+    );
+
+    protected $serviceColors = array(
+        0 => array('black', 'lightgreen'),
+        1 => array('black', 'yellow'),
+        2 => array('black', 'lightred'),
+        3 => array('black', 'lightpurple'),
+        99 => array('black', 'lightgray'),
+    );
 
     public function init()
     {
@@ -49,8 +66,44 @@ class CheckCommand extends Command
             );
             exit(3);
         }
+
         printf("Business Process %s: %s\n", $node->getStateName(), $node->getAlias());
+        if ($this->params->shift('details')) {
+            echo $this->renderProblemTree($node->getProblemTree(), $this->params->shift('colors'));
+        }
+
         exit($node->getState());
+    }
+
+    protected function renderProblemTree($tree, $useColors = false, $depth = 0)
+    {
+        $output = '';
+
+        foreach ($tree as $name => $subtree) {
+            $node = $subtree['node'];
+
+            if ($node instanceof HostNode) {
+                $colors = $this->hostColors[$node->getState()];
+            } else {
+                $colors = $this->serviceColors[$node->getState()];
+            }
+
+            $state = sprintf('[%s]', $node->getStateName());
+            if ($useColors) {
+                $state = $this->screen->colorize($state, $colors[0], $colors[1]);
+            }
+
+            $output .= sprintf(
+                "%s%s %s %s\n",
+                str_repeat('  ', $depth),
+                $node instanceof BpNode ? $node->getOperator() : '-',
+                $state,
+                $node->getAlias()
+            );
+            $output .= $this->renderProblemTree($subtree['children'], $useColors, $depth + 1);
+        }
+
+        return $output;
     }
 
     protected function getFirstProcessName()
