9e17ff21ba6ceec19d57a4c429727ef807d4cd4f
Thomas Gelf <thomas@gelf.net>
2016-11-23 10:12:25 +0100

    Test: add base test framework

---
+++ library/Businessprocess/Test/BaseTestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Icinga\Module\Businessprocess\Test;
+
+use Icinga\Application\ApplicationBootstrap;
+use Icinga\Application\Icinga;
+use PHPUnit_Framework_TestCase;
+
+abstract class BaseTestCase extends PHPUnit_Framework_TestCase
+{
+    /** @var ApplicationBootstrap */
+    private static $app;
+
+    /**
+     * @inheritdoc
+     */
+    public function setUp()
+    {
+        $this->app();
+    }
+
+    /**
+     * @param null $subDir
+     * @return string
+     */
+    protected function getTestsBaseDir($subDir = null)
+    {
+        $dir = dirname(dirname(dirname(__DIR__))) . '/test';
+        if ($subDir === null) {
+            return $dir;
+        } else {
+            return $dir . '/' . ltrim($subDir, '/');
+        }
+    }
+
+    /**
+     * @return ApplicationBootstrap
+     */
+    protected function app()
+    {
+        if (self::$app === null) {
+            self::$app = Icinga::app();
+        }
+
+        return self::$app;
+    }
+}
