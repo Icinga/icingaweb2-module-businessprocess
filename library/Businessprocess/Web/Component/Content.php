162c19c023fd096323115aa216526fadbe5eca2f
Thomas Gelf <thomas@gelf.net>
2016-11-28 22:20:01 +0100

    Content, Controls: add new convenience classes

---
+++ library/Businessprocess/Web/Component/Content.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Icinga\Module\Businessprocess\Web\Component;
+
+use Icinga\Module\Businessprocess\Html\Container;
+
+class Content extends Container
+{
+    protected $contentSeparator = "\n";
+
+    protected $defaultAttributes = array('class' => 'content');
+}
---
+++ library/Businessprocess/Web/Component/Controls.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Icinga\Module\Businessprocess\Web\Component;
+
+use Icinga\Module\Businessprocess\Html\Container;
+
+class Controls extends Container
+{
+    protected $contentSeparator = "\n";
+
+    protected $defaultAttributes = array('class' => 'controls');
+}
