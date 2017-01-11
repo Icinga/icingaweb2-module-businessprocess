d8298417e42b7ad137aaeac4709c365d8803571e
Thomas Gelf <thomas@gelf.net>
2017-01-11 13:37:21 +0100

    TextTest: more tests

--- test/php/library/Businessprocess/Html/TextTest.php
+++ test/php/library/Businessprocess/Html/TextTest.php
@@ -15,6 +15,24 @@ class TextTest extends BaseTestCase
         );
     }
 
+    public function testTextIsRenderedAsGivenWhenDeclaredBeingEscaped()
+    {
+        $this->assertEquals(
+            'A & O',
+            Text::create('A & O')->setEscaped()->render()
+        );
+
+        $this->assertEquals(
+            'A & O',
+            Text::create('A & O')->setEscaped(true)->render()
+        );
+
+        $this->assertEquals(
+            'A &amp; O',
+            Text::create('A & O')->setEscaped(false)->render()
+        );
+    }
+
     public function testTextIsEscapedWhenRendered()
     {
         $this->assertEquals(
