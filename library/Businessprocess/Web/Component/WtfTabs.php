6d66ebd72a4d2f8f7bf27bba5b642db7ec36e0dc
Thomas Gelf <thomas@gelf.net>
2017-01-20 13:29:01 +0100

    WtfTabs: add a comment exlaining this WtfClass

--- library/Businessprocess/Web/Component/WtfTabs.php
+++ library/Businessprocess/Web/Component/WtfTabs.php
@@ -4,6 +4,15 @@ namespace Icinga\Module\Businessprocess\Web\Component;
 
 use Icinga\Web\Widget\Tabs;
 
+/**
+ * Class WtfTabs
+ *
+ * TODO: Please remove this as soon as we drop support for PHP 5.3.x
+ *       This works around https://bugs.php.net/bug.php?id=43200 and fixes
+ *       https://github.com/Icinga/icingaweb2-module-businessprocess/issues/81
+ *
+ * @package Icinga\Module\Businessprocess\Web\Component
+ */
 class WtfTabs extends Tabs
 {
     public function render()
