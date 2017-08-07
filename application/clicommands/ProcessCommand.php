7dea7f6a87eddcca4b337f56938effab7caa4025
Stefan Brinkmann <stefan.brinkmann@payback.net>
2017-08-07 15:16:53 +0200

    ProcessCommand: add action 'handle' to get handle state

--- application/clicommands/ProcessCommand.php
+++ application/clicommands/ProcessCommand.php
@@ -72,6 +72,7 @@ class ProcessCommand extends Command
      *   --config <configname>   Name of the config that contains <process>
      *   --details               Show problem details as a tree
      *   --colors                Show colored output
+     *   --handled               Show global Acknowledge handle state 
      *   --state-type <type>     Define which state type to look at. Could be
      *                           either soft or hard, overrides an eventually
      *                           configured default
@@ -107,6 +108,14 @@ class ProcessCommand extends Command
         }
 
         printf("Business Process %s: %s\n", $node->getStateName(), $node->getAlias());
+        if($this->params->shift('handled')) {
+           if($node->isHandled()) {
+             $handleState = "TRUE";
+           } else {
+             $handleState = "FALSE";
+           }
+           printf("Acknowledged: %s\n", $handleState);
+        }
         if ($this->params->shift('details')) {
             echo $this->renderProblemTree($node->getProblemTree(), $this->params->shift('colors'));
         }
