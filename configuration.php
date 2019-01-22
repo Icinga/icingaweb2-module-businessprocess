a3ce6e1543c1a043b8abf2304895a81e605ec971
Johannes Meyer <johannes.meyer@netways.de>
2019-01-22 11:45:35 +0100

    tree: Apply final design

--- configuration.php
+++ configuration.php
@@ -61,3 +61,5 @@ $this->provideRestriction(
 $this->provideJsFile('vendor/Sortable.js');
 $this->provideJsFile('behavior/sortable.js');
 $this->provideJsFile('vendor/jquery.fn.sortable.js');
+
+$this->provideCssFile('state-ball.less');
