af4226aa436b95f5ca78a29e9eca4d1d03f1c2e9
Ravi Kumar Kempapura Srinivasa <ravi.srinivasa@icinga.com>
2021-09-28 13:58:28 +0200

    Address Node which is UP or OK with downtime.

--- library/Businessprocess/BpNode.php
+++ library/Businessprocess/BpNode.php
@@ -74,7 +74,7 @@ class BpNode extends Node
                     $this->counters['MISSING']++;
                 } else {
                     $state = $child->getStateName($this->getChildState($child));
-                    if ($child->isHandled()) {
+                    if ($child->isHandled() && ($state !== 'UP' && $state !== 'OK')) {
                         $state = $state . '-HANDLED';
                     }
 
