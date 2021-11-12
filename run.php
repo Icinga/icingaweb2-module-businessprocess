08175ae652e217787bf1e0c24be1c0f6f72183fe
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2021-11-12 12:02:33 +0100

    Provide hook for `icingadbSupport`

--- run.php
+++ run.php
@@ -2,4 +2,5 @@
 
 $this->provideHook('monitoring/HostActions');
 $this->provideHook('monitoring/ServiceActions');
+$this->provideHook('icingadb/icingadbSupport');
 //$this->provideHook('director/shipConfigFiles');
