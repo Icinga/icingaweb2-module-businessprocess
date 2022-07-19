b2bc6b852e3b924df9b5cf890126e1be2d284b92
Johannes Meyer <johannes.meyer@icinga.com>
2022-07-19 16:19:07 +0200

    Enhance DetailViewExtension
    * The checkcommand name can now be configured
    * The variable names are shortened
    * The config and process variables are now separate

--- library/Businessprocess/ProvidedHook/Monitoring/DetailviewExtension.php
+++ library/Businessprocess/ProvidedHook/Monitoring/DetailviewExtension.php
@@ -13,10 +13,11 @@ use Icinga\Module\Monitoring\Object\Service;
 
 class DetailviewExtension extends DetailviewExtensionHook
 {
-    /**
-     * @var $storage LegacyStorage
-     */
+    /** @var LegacyStorage */
     private $storage;
+
+    /** @var string */
+    private $commandName;
 
     /**
      * Initialize storage
@@ -24,8 +25,11 @@ class DetailviewExtension extends DetailviewExtensionHook
     public function init()
     {
         try {
-            $this->storage = new LegacyStorage(
-                $this->getModule()->getConfig()->getSection('global')
+            $this->storage = LegacyStorage::getInstance();
+            $this->commandName = $this->getModule()->getConfig()->get(
+                'DetailviewExtension',
+                'checkcommand_name',
+                'icingacli-businessprocess'
             );
         } catch (\Exception $e) {
             // Ignore and don't display anything
@@ -43,29 +47,35 @@ class DetailviewExtension extends DetailviewExtensionHook
     {
         if (! isset($this->storage)
             || ! $object instanceof Service
-            || $object->check_command !== 'icingacli-businessprocess'
+            || $object->check_command !== $this->commandName
         ) {
             return '';
         }
 
-        $bpName = $object->_service_icingacli_businessprocess_process;
+        $bpName = $object->_service_businessprocess_config;
         if (! $bpName) {
             return '';
         }
 
+        $nodeName = $object->_service_businessprocess_process;
+        if (! $nodeName) {
+            return '';
+        }
+
         $bp = $this->storage->loadProcess($bpName);
+        $node = $bp->getBpNode($nodeName);
 
         MonitoringState::apply($bp);
 
-        if (filter_var($object->_service_icingacli_businessprocess_grapher_tree, FILTER_VALIDATE_BOOLEAN)) {
-            $renderer = new TreeRenderer($bp);
+        if (filter_var($object->_service_businessprocess_as_tree, FILTER_VALIDATE_BOOLEAN)) {
+            $renderer = new TreeRenderer($bp, $node);
             $tag = 'ul';
         } else {
-            $renderer = new TileRenderer($bp);
+            $renderer = new TileRenderer($bp, $node);
             $tag = 'div';
         }
 
-        $renderer->setUrl(Url::fromPath('businessprocess/process/show?config=' . $bpName . '&node=' . $bpName));
+        $renderer->setUrl(Url::fromPath('businessprocess/process/show?config=' . $bpName . '&node=' . $nodeName));
         $renderer->ensureAssembled()->getFirst($tag)->setAttribute('data-base-target', '_next');
 
         return '<h2>Business Process</h2>' . $renderer;
