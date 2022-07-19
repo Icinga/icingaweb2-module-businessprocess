b8de4580da41201a341b4d112fa30c83469d8ef8
Johannes Meyer <johannes.meyer@icinga.com>
2022-07-19 16:20:56 +0200

    Add detail integration for Icinga DB Web

---
+++ library/Businessprocess/ProvidedHook/Icingadb/ServiceDetailExtension.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Icinga\Module\Businessprocess\ProvidedHook\Icingadb;
+
+use Icinga\Module\Businessprocess\Renderer\TileRenderer;
+use Icinga\Module\Businessprocess\Renderer\TreeRenderer;
+use Icinga\Module\Businessprocess\State\IcingaDbState;
+use Icinga\Module\Businessprocess\Storage\LegacyStorage;
+use Icinga\Module\Businessprocess\Web\Url;
+use Icinga\Module\Icingadb\Hook\ServiceDetailExtensionHook;
+use Icinga\Module\Icingadb\Model\Service;
+use ipl\Html\Html;
+use ipl\Html\HtmlDocument;
+use ipl\Html\HtmlString;
+use ipl\Html\ValidHtml;
+
+class ServiceDetailExtension extends ServiceDetailExtensionHook
+{
+    /** @var LegacyStorage */
+    private $storage;
+
+    /** @var string */
+    private $commandName;
+
+    protected function init()
+    {
+        $this->setSection(self::GRAPH_SECTION);
+
+        try {
+            $this->storage = LegacyStorage::getInstance();
+            $this->commandName = $this->getModule()->getConfig()->get(
+                'DetailviewExtension',
+                'checkcommand_name',
+                'icingacli-businessprocess'
+            );
+        } catch (\Exception $e) {
+            // Ignore and don't display anything
+        }
+    }
+
+    public function getHtmlForObject(Service $service): ValidHtml
+    {
+        if (! isset($this->storage)
+            || $service->checkcommand_name !== $this->commandName
+        ) {
+            return HtmlString::create('');
+        }
+
+        $bpName = $service->customvars['businessprocess_config'] ?? null;
+        if (! $bpName) {
+            return HtmlString::create('');
+        }
+
+        $nodeName = $service->customvars['businessprocess_process'] ?? null;
+        if (! $nodeName) {
+            return HtmlString::create('');
+        }
+
+        $bp = $this->storage->loadProcess($bpName);
+        $node = $bp->getBpNode($nodeName);
+
+        IcingaDbState::apply($bp);
+
+        if ($service->customvars['businessprocess_as_tree'] ?? false) {
+            $renderer = new TreeRenderer($bp, $node);
+            $tag = 'ul';
+        } else {
+            $renderer = new TileRenderer($bp, $node);
+            $tag = 'div';
+        }
+
+        $renderer->setUrl(Url::fromPath('businessprocess/process/show?config=' . $bpName . '&node=' . $nodeName));
+        $renderer->ensureAssembled()->getFirst($tag)->setAttribute('data-base-target', '_next');
+
+        return (new HtmlDocument())->addHtml(Html::tag('h2', 'Business Process'), $renderer);
+    }
+}
