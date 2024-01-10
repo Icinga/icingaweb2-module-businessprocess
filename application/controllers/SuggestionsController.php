ae460426e98cd4aff6547a38d6e32c1eb7451701
MoreAmazingNick <github@nicolas-schneider.at>
2024-01-10 16:29:52 +0100

    Fix indistinguishable suggestions for nodes refs #427

--- application/controllers/SuggestionsController.php
+++ application/controllers/SuggestionsController.php
@@ -101,7 +101,7 @@ class SuggestionsController extends Controller
                     ) {
                         yield [
                             'search' => $search,
-                            'label'  => $bpNode->getAlias() ?? $bpNode->getName(),
+                            'label'  => ($bpNode->getAlias() ?? $bpNode->getName())." (".implode("->",$bpNode->getPaths()[0]).")",
                             'config' => $config
                         ];
                     }
