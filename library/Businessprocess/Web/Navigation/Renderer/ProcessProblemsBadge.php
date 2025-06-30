b9dc434baea799217480b49c235851fcc7d896b8
Johannes Meyer <johannes.meyer@icinga.com>
2025-06-30 10:04:19 +0200

    Do not break sidebar if processes cannot be loaded
    fixes #466
    
    (cherry picked from commit a6033491f42c99df72e338a0729f6403bc3393be)

--- library/Businessprocess/Web/Navigation/Renderer/ProcessProblemsBadge.php
+++ library/Businessprocess/Web/Navigation/Renderer/ProcessProblemsBadge.php
@@ -3,8 +3,10 @@
 namespace Icinga\Module\Businessprocess\Web\Navigation\Renderer;
 
 use Icinga\Module\Businessprocess\Node;
+use Icinga\Application\Logger;
 use Icinga\Module\Businessprocess\Storage\LegacyStorage;
 use Icinga\Web\Navigation\Renderer\BadgeNavigationItemRenderer;
+use Throwable;
 
 class ProcessProblemsBadge extends BadgeNavigationItemRenderer
 {
@@ -20,22 +22,29 @@ class ProcessProblemsBadge extends BadgeNavigationItemRenderer
 
     public function getCount()
     {
-        $count = 0;
         if ($this->count === null) {
-            $storage = LegacyStorage::getInstance();
-            $bp = $storage->loadProcess($this->getBpConfigName());
-            foreach ($bp->getRootNodes() as $rootNode) {
-                if (! $rootNode->isEmpty() &&
-                    ! in_array($rootNode->getState(), [Node::ICINGA_OK, Node::ICINGA_PENDING], true)) {
-                    $count++;
+            $count = 0;
+
+            try {
+                $storage = LegacyStorage::getInstance();
+
+                // State is not applied here, because it's already done in ProcessesProblemsBadge.
+                // It runs earlier as it is part of the parent menu entry. Do we rely on an implementation detail here?
+                // Probably, but it is how it is for a very long time. So it is unlikely to change. (Finger crossed.)
+                $bp = $storage->loadProcess($this->getBpConfigName());
+                foreach ($bp->getRootNodes() as $rootNode) {
+                    if (! $rootNode->isEmpty() &&
+                        ! in_array($rootNode->getState(), [Node::ICINGA_OK, Node::ICINGA_PENDING], true)) {
+                        $count++;
+                    }
                 }
+            } catch (Throwable $e) {
+                Logger::error('Failed to load business process "%s": %s', $this->getBpConfigName(), $e);
             }
 
             $this->count = $count;
-            $this->setState(self::STATE_CRITICAL);
-        }
 
-        if ($count) {
+            $this->setState(self::STATE_CRITICAL);
             $this->setTitle(sprintf(
                 tp('One unhandled root node critical', '%d unhandled root nodes critical', $count),
                 $count
